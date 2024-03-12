4ffd04c360215766e126757686516b35afbc1f7c
Holden <demin_012@mail.ru>
2024-03-13 00:20:36 +0100

    crossover, mutation, fitness added to TSP

--- TSP.py
+++ TSP.py
@@ -1,7 +1,25 @@
 import math
-import random
-
+import random as r
 import numpy as np
+
+from EvolutionaryAlg import EvolutionaryAlgorithm
+
+
+class CityPath:
+
+    def __init__(self, path, distance_matrix):
+        self.path = path
+        self.distance_matrix = np.array(distance_matrix)
+
+    def get_path_distance(self):
+        distance = 0
+        for i in range(len(self.path) - 1):
+            distance += self.distance_matrix[self.path[i] - 1][self.path[i + 1] - 1]
+
+        return distance
+
+    def __len__(self):
+        return len(self.path)
 
 
 class TSP:
@@ -17,12 +35,13 @@ class TSP:
     def generate_population(self):
         population = []
         for i in range(self.population_size):
-            population.append(self.generate_path())
+            population.append(CityPath(path=self.generate_path(),
+                                       distance_matrix=self.distance_matrix))
         return population
 
     def generate_path(self):
         city_to_visit = [*range(1, self.num_cities + 1)]
-        path = random.sample(city_to_visit, self.num_cities)
+        path = r.sample(city_to_visit, self.num_cities)
         return path
 
     def generate_locations(self):
@@ -33,10 +52,10 @@ class TSP:
     @staticmethod
     def distance(angle1, angle2):
         abs_diff = abs(angle1 - angle2)
-        gamma = min(abs_diff, 360 - abs_diff) # choose an acute/straight angle
+        gamma = min(abs_diff, 360 - abs_diff)  # choose an acute/straight angle
         gamma_radians = math.radians(gamma)
         distance = math.sqrt(2 - 2 * math.cos(gamma_radians))  # cosines theorem
-        return round(distance,2)
+        return round(distance, 2)
 
     def build_distance_matrix(self):
         """ Creates a matrix for quick distance lookups
@@ -53,14 +72,70 @@ class TSP:
         return distance_matrix
 
     def generate_city_angle(self):
-        return round(random.uniform(0,1) * 360, 2)
+        return round(r.uniform(0, 1) * 360, 2)
 
-    def fitness_function(self, candidate):
-        pass
+    @staticmethod
+    def fitness_function(city_path):
+        return city_path.get_path_distance()
+
+    @staticmethod
+    def partially_mapped_crossover(parent1, parent2):
+        cutpoints = r.randint(0, len(parent1) - 1), r.randint(0, len(parent2) - 1)
+        cutpoint1 = min(cutpoints)
+        cutpoint2 = max(cutpoints)
+
+        p1 = parent1.path
+        p2 = parent2.path
+
+        size = len(parent1)
+        child1_path = [None] * cutpoint1 + p2[cutpoint1:cutpoint2 + 1] + [None] * (size - cutpoint2 - 1)
+        child2_path = [None] * cutpoint1 + p1[cutpoint1:cutpoint2 + 1] + [None] * (size - cutpoint2 - 1)
+
+        for i in range(size):
+            if not (cutpoint1 <= i <= cutpoint2):
+                # child 1: copy bits without conflicts, the rest is substituted with mappings
+                if not p1[i] in child1_path:
+                    child1_path[i] = p1[i]
+                else:
+                    chosen_bit = p1[i]
+                    while chosen_bit in child1_path:
+                        chosen_bit = p1[p2.index(chosen_bit)]
+                    child1_path[i] = chosen_bit
+
+                # do the same for child 2
+                if not p2[i] in child2_path:
+                    child2_path[i] = p2[i]
+                else:
+                    chosen_bit = p2[i]
+                    while chosen_bit in child2_path:
+                        chosen_bit = p2[p1.index(chosen_bit)]
+                    child2_path[i] = chosen_bit
+
+        dm = parent1.distance_matrix
+        return [CityPath(child1_path, dm), CityPath(child2_path, dm)]
+
+    @staticmethod
+    def swap_mutation(child):
+        swap_pts = r.randint(0, len(child) - 1), r.randint(0, len(child) - 1)
+        child_p = child.path
+        child_p[swap_pts[0]], child_p[swap_pts[1]] = child_p[swap_pts[1]], child_p[swap_pts[0]]
+        child.path = child_p
+        return child
+
+    def solve(self):
+        genetic_algo = EvolutionaryAlgorithm(population=self.population, fitness_function=self.fitness_function)
+        genetic_algo.custom_crossover = TSP.partially_mapped_crossover
+        genetic_algo.custom_mutation = TSP.swap_mutation
+        best_path = genetic_algo.evolve(50)
+        print(genetic_algo.scores)
+        print(f"Best score: {genetic_algo.best_score}")
+        print(
+            f"Best CityPath \ndistance: {round(best_path.get_path_distance())}")
+        print([self.locations[i] for i in best_path.path])
 
 
 if __name__ == "__main__":
-    tsp = TSP(num_cities=10, population_size=10, num_generations=50)
+    tsp = TSP(num_cities=10, population_size=100, num_generations=100)
 
     # tests
     print(tsp.locations)
@@ -69,3 +144,20 @@ if __name__ == "__main__":
     print(tsp.distance(45, 330))
     print(tsp.distance(0, 180))
     print(np.array(tsp.distance_matrix))
+    print(tsp.fitness_function(tsp.population[0]))
+
+    parent1 = CityPath([3, 4, 8, 2, 7, 1, 6, 5], [])
+    parent2 = CityPath([4, 2, 5, 1, 6, 8, 3, 7], [])
+    child = CityPath([1,2,3,4,5],[])
+
+    c1, c2 = tsp.partially_mapped_crossover(parent1, parent2)
+    print(c1.path)
+    print(c2.path)
+    print(f'c1: {c1.path}')
+    print(f'c2: {c2.path}')
+
+    print(tsp.swap_mutation(child).path)
+
+    tsp.solve()
+
+
