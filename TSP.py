ba5471229e4bc14c502ab100b079fbbeb726118e
Holden <demin_012@mail.ru>
2024-03-14 23:54:15 +0100

    added a distance from -1st to 1st element in calcs of a path's distance

--- TSP.py
+++ TSP.py
@@ -16,6 +16,8 @@ class CityPath:
         for i in range(len(self.path) - 1):
             distance += self.distance_matrix[self.path[i] - 1][self.path[i + 1] - 1]
 
+        distance += self.distance_matrix[self.path[-1] - 1][self.path[0]-1]
+
         return distance
 
     def __len__(self):
@@ -45,9 +47,13 @@ class TSP:
         return path
 
     def generate_locations(self):
-        locations = [self.generate_city_angle() for _ in range(self.num_generations)]
+        locations = [self.generate_city_angle() for _ in range(self.num_cities)]
         locations_sorted = sorted(locations)
         return locations_sorted
+
+    @staticmethod
+    def generate_city_angle():
+        return round(r.uniform(0, 1) * 360, 2)
 
     @staticmethod
     def distance(angle1, angle2):
@@ -71,12 +77,10 @@ class TSP:
 
         return distance_matrix
 
-    def generate_city_angle(self):
-        return round(r.uniform(0, 1) * 360, 2)
-
     @staticmethod
     def fitness_function(city_path):
-        return city_path.get_path_distance()
+        fitness = city_path.get_path_distance()
+        return fitness
 
     @staticmethod
     def partially_mapped_crossover(parent1, parent2):
@@ -123,41 +127,54 @@ class TSP:
         return child
 
     def solve(self):
-        genetic_algo = EvolutionaryAlgorithm(population=self.population, fitness_function=self.fitness_function)
+        genetic_algo = EvolutionaryAlgorithm(population=self.population, fitness_function=self.fitness_function, maximization=False)
         genetic_algo.custom_crossover = TSP.partially_mapped_crossover
         genetic_algo.custom_mutation = TSP.swap_mutation
-        best_path = genetic_algo.evolve(50)
-        print(genetic_algo.scores)
+        best_path = genetic_algo.evolve(self.num_generations)
         print(f"Best score: {genetic_algo.best_score}")
         print(
-            f"Best CityPath \ndistance: {round(best_path.get_path_distance())}")
-        print([self.locations[i] for i in best_path.path])
+            f"Best CityPath \ndistance: {round(best_path.get_path_distance(),2)}"
+            f"\nbest_path.path: {best_path.path}")
+        print([self.locations[i-1] for i in best_path.path])
+
+        optimal_distance = 0
+        for i in range(len(self.locations) - 1):
+            optimal_distance += self.distance_matrix[i][i+1]
+        optimal_distance += self.distance_matrix[-1][0]
+        print(f"Optimal path: {optimal_distance}")
 
 
 if __name__ == "__main__":
-    tsp = TSP(num_cities=10, population_size=100, num_generations=100)
+    r.seed(42)
+
+    # parameters
+    num_cities = 100
+    population_size = 1000
+    num_generations = 1000
+
+    tsp = TSP(num_cities=num_cities, population_size=population_size, num_generations=num_generations)
+
+    # run
+    tsp.solve()
 
     # tests
-    print(tsp.locations)
-    print(tsp.population[0])
-    print(tsp.distance(tsp.locations[0], tsp.locations[1]))
-    print(tsp.distance(45, 330))
-    print(tsp.distance(0, 180))
-    print(np.array(tsp.distance_matrix))
-    print(tsp.fitness_function(tsp.population[0]))
-
-    parent1 = CityPath([3, 4, 8, 2, 7, 1, 6, 5], [])
-    parent2 = CityPath([4, 2, 5, 1, 6, 8, 3, 7], [])
-    child = CityPath([1,2,3,4,5],[])
-
-    c1, c2 = tsp.partially_mapped_crossover(parent1, parent2)
-    print(c1.path)
-    print(c2.path)
-    print(f'c1: {c1.path}')
-    print(f'c2: {c2.path}')
-
-    print(tsp.swap_mutation(child).path)
-
-    tsp.solve()
+    # print(tsp.locations)
+    # print(tsp.population[0])
+    # print(tsp.distance(tsp.locations[0], tsp.locations[1]))
+    # print(tsp.distance(45, 330))
+    # print(tsp.distance(0, 180))
+    # print(np.array(tsp.distance_matrix))
+    # print(tsp.fitness_function(tsp.population[0]))
+    #
+    # parent1 = CityPath([3, 4, 8, 2, 7, 1, 6, 5], [])
+    # parent2 = CityPath([4, 2, 5, 1, 6, 8, 3, 7], [])
+    # child = CityPath([1,2,3,4,5],[])
+    #
+    # c1, c2 = tsp.partially_mapped_crossover(parent1, parent2)
+    # print(c1.path)
+    # print(c2.path)
+    # print(f'c1: {c1.path}')
+    # print(f'c2: {c2.path}')
+    # print(tsp.swap_mutation(child).path)
 
 
